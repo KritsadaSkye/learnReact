e4cd0b349d354b6ceaf166483bc12c58f327fbe8
KritsadaSkye <121706811+KritsadaSkye@users.noreply.github.com>
2025-09-30 16:18:47 +0700

    Use axios to fetch the data.

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './Homepage.css';
 
 export function HomePage() {
+    axios.get('http://localhost:3000/api/products').then((response) => {
+        console.log(response.data);
+    });
+
     return (
         <>
             <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
